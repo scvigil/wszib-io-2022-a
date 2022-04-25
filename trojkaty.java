245164a0ef19a52e32ca5178daa3c2c0064b57f3
Szymon <szymon.warta@outlook.com>
2022-04-25 16:14:31 +0200

    Nowa funkcjonalność: trójkąt równoramienny

--- trojkaty.java
+++ trojkaty.java
@@ -10,8 +10,14 @@ class Trojkaty {
      * @param {float} c - Długość trzeciego boku.
      */
     public static void jakiTrojkat(float a, float b, float c){
-         if (a == b && b == c) {
+        if (a == b && b == c) {
              System.out.println("Trójkąt równoboczny"); 
+         }
+	if (a == b && b == c && a == c) {
+        	System.out.println("Trójkąt równoboczny");
+         }
+	if (a == b || b == c || a == c) {
+        	System.out.println("Trójkąt równoramienny");
          }
          // TODO: tutaj trzeba bedzie dopisac inne przypadki
     }
