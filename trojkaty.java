23a7315bd30169e8777f41cfaae3d95d9218dd4d
Krzysztof Wróbel <calwera@gmail.com>
2022-05-30 12:26:50 +0200

    Optymalizacja: uwspólnienie kodu

--- trojkaty.java
+++ trojkaty.java
@@ -9,16 +9,28 @@ class Trojkaty {
      * @param {float} b - Długość drugiego boku.
      * @param {float} c - Długość trzeciego boku.
      */
+
+	 public static int ileJednakowych(float a, float b, float c){
+                   int jednakowe = 0; // tutaj zliczamy ile jest jednakowych bokow
+                   if (a == b) {
+                      jednakowe++;
+                   }
+                   if (b == c) {
+                      jednakowe++;
+                   }
+                   return jednakowe;
+               }
+
+		
     public static void jakiTrojkat(float a, float b, float c){
-        if (a == b && b == c) {
-             System.out.println("Trójkąt równoboczny"); 
+        int jednakoweDlugosci =  ileJednakowych(a,b,c); //  tutaj zliczamy ile jest jednakowych bokow
+	if (jednakoweDlogosci > 1) {          
+   System.out.println("Trójkąt równoboczny"); 
          }
-	if (a == b && b == c && a == c) {
-        	System.out.println("Trójkąt równoboczny");
-         }
-	if (a == b || b == c || a == c) {
+      else if (jednakoweDlugosci > 0) {
         	System.out.println("Trójkąt równoramienny");
          }
+	
          // TODO: tutaj trzeba bedzie dopisac inne przypadki
     }
     /** Wyświetla ekran pomocy */
@@ -31,7 +43,7 @@ class Trojkaty {
     public static void main(String... args) {
         if (args.length != 3) {
             pomoc();
-            System.exit(1);
+             System.exit(1);
         }
         float a = Float.valueOf(args[0]);
         float b = Float.valueOf(args[1]);
